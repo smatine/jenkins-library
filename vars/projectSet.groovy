426910e711a714ed854381f10cfdfd38dcdb2538
smatine <soufiane.matine@gmail.com>
2017-11-21 00:05:41 +0100

    Update projectSet.groovy

--- vars/projectSet.groovy
+++ vars/projectSet.groovy
@@ -3,5 +3,5 @@
 def call(project) {
     sh "oc new-project ${project} || echo 'Project exists'"
     sh "oc project ${project}"
-    sh "oc policy add-role-to-user admin developer -n ${project}"
+    sh "oc policy add-role-to-user admin developer centos -n ${project}"
 }
